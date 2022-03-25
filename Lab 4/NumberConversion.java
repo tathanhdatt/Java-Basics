e047961aa5d860bbe2966d3dcd47badd6f94bb82
Lawliet529 <85656044+Lawliet529@users.noreply.github.com>
2022-03-25 21:10:39 +0700

    Optimize NumberConversion.java
    Don't reinvent the wheel. :)

--- Lab 4/NumberConversion.java
+++ Lab 4/NumberConversion.java
@@ -21,36 +21,9 @@ public class NumberConversion {
   }
 
   public static String toRadix(String inStr, int inRadix, int outRadix) {
-    inStr = inStr.toUpperCase();
-
-    // convert input radix to decimal
-    int numDec = 0;
-    int idx = 0;
-    while (idx < inStr.length()) {
-      char inChar = inStr.charAt(inStr.length() - idx - 1);
-      if ('0' <= inChar && inChar <= '9') {
-        numDec += Math.pow(inRadix, idx) * Character.getNumericValue(inChar);
-      } else {
-        numDec += Math.pow(inRadix, idx) * (inChar - 55);
-      }
-      idx++;
-    }
-
-    if (outRadix == 10 ) {
-      return String.valueOf(numDec);
-    }
-
-    //convert decimal to output radix
-    StringBuilder result = new StringBuilder();
-    while (numDec > 0) {
-      int term = numDec % outRadix;
-      if (term <= 9) {
-        result.insert(0, term);
-      } else {
-        result.insert(0, (char) (term + 55));
-      }
-      numDec /= outRadix;
-    }
-    return result.toString();
+        // Convert to decimal
+        int decimal = Integer.parseInt(inStr, inRadix);
+        // Convert to radix
+        return Integer.toString(decimal, outRadix);
   }
 }
